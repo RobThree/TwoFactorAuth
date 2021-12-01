5f2611cde2dad49f9d79343264980f9fa58e1bd0
Matthew Peveler <matt.peveler@popsql.com>
2021-12-01 09:44:40 -0500

    Fix compatibility with endroid/qr-code 4

--- lib/Providers/Qr/EndroidQrCodeProvider.php
+++ lib/Providers/Qr/EndroidQrCodeProvider.php
@@ -1,8 +1,14 @@
 <?php
 namespace RobThree\Auth\Providers\Qr;
 
+use Endroid\QrCode\Color\Color;
 use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelLow;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelMedium;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelQuartile;
 use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
 
 class EndroidQrCodeProvider implements IQRCodeProvider
 {
@@ -11,8 +17,12 @@ class EndroidQrCodeProvider implements IQRCodeProvider
     public $margin;
     public $errorcorrectionlevel;
 
+    protected $endroid4 = false;
+
     public function __construct($bgcolor = 'ffffff', $color = '000000', $margin = 0, $errorcorrectionlevel = 'H')
     {
+        $this->endroid4 = method_exists(QrCode::class, 'create');
+
         $this->bgcolor = $this->handleColor($bgcolor);
         $this->color = $this->handleColor($color);
         $this->margin = $margin;
@@ -26,7 +36,12 @@ class EndroidQrCodeProvider implements IQRCodeProvider
 
     public function getQRCodeImage($qrtext, $size)
     {
-        return $this->qrCodeInstance($qrtext, $size)->writeString();
+        if (!$this->endroid4) {
+            return $this->qrCodeInstance($qrtext, $size)->writeString();
+        }
+
+        $writer = new PngWriter();
+        return $writer->write($this->qrCodeInstance($qrtext, $size))->getString();
     }
 
     protected function qrCodeInstance($qrtext, $size)
@@ -49,22 +64,21 @@ class EndroidQrCodeProvider implements IQRCodeProvider
         $g = hexdec($split[1]);
         $b = hexdec($split[2]);
 
-        return ['r' => $r, 'g' => $g, 'b' => $b, 'a' => 0];
+        return $this->endroid4 ? new Color($r, $g, $b, 0) : ['r' => $r, 'g' => $g, 'b' => $b, 'a' => 0];
     }
 
     private function handleErrorCorrectionLevel($level)
     {
         switch ($level) {
             case 'L':
-                return ErrorCorrectionLevel::LOW();
+                return $this->endroid4 ? new ErrorCorrectionLevelLow() : ErrorCorrectionLevel::LOW();
             case 'M':
-                return ErrorCorrectionLevel::MEDIUM();
+                return $this->endroid4 ? new ErrorCorrectionLevelMedium() : ErrorCorrectionLevel::MEDIUM();
             case 'Q':
-                return ErrorCorrectionLevel::QUARTILE();
+                return $this->endroid4 ? new ErrorCorrectionLevelQuartile() : ErrorCorrectionLevel::QUARTILE();
             case 'H':
-                return ErrorCorrectionLevel::HIGH();
             default:
-                return ErrorCorrectionLevel::HIGH();
+                return $this->endroid4 ? new ErrorCorrectionLevelHigh() : ErrorCorrectionLevel::HIGH();
         }
     }
 }
--- lib/Providers/Qr/EndroidQrCodeWithLogoProvider.php
+++ lib/Providers/Qr/EndroidQrCodeWithLogoProvider.php
@@ -1,8 +1,8 @@
 <?php
 namespace RobThree\Auth\Providers\Qr;
 
-use Endroid\QrCode\ErrorCorrectionLevel;
-use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Logo\Logo;
+use Endroid\QrCode\Writer\PngWriter;
 
 class EndroidQrCodeWithLogoProvider extends EndroidQrCodeProvider
 {
@@ -20,13 +20,33 @@ class EndroidQrCodeWithLogoProvider extends EndroidQrCodeProvider
         $this->logoSize = (array)$size;
     }
 
+    public function getQRCodeImage($qrtext, $size)
+    {
+        if (!$this->endroid4) {
+            return $this->qrCodeInstance($qrtext, $size)->writeString();
+        }
+
+        $logo = null;
+        if ($this->logoPath) {
+            $logo = Logo::create($this->logoPath);
+            if ($this->logoSize) {
+                $logo->setResizeToWidth($this->logoSize[0]);
+                if (isset($this->logoSize[1])) {
+                    $logo->setResizeToHeight($this->logoSize[1]);
+                }
+            }
+        }
+        $writer = new PngWriter();
+        return $writer->write($this->qrCodeInstance($qrtext, $size), $logo)->getString();
+    }
+
     protected function qrCodeInstance($qrtext, $size) {
         $qrCode = parent::qrCodeInstance($qrtext, $size);
 
-        if ($this->logoPath) {
+        if (!$this->endroid4 && $this->logoPath) {
             $qrCode->setLogoPath($this->logoPath);
             if ($this->logoSize) {
-                $qrCode->setLogoSize($this->logoSize[0], $this->logoSize[1]);
+                $qrCode->setLogoSize($this->logoSize[0], isset($this->logoSize[1]) ? $this->logoSize[1] : null);
             }
         }
 
