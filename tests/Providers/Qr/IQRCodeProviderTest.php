4f1543f7828a5c620666701a99535372238ddd6b
William Hall <github@willpoweredinc.co.uk>
2021-12-19 18:16:38 +0000

    🚨 add test to cover null issuer

--- tests/Providers/Qr/IQRCodeProviderTest.php
+++ tests/Providers/Qr/IQRCodeProviderTest.php
@@ -43,6 +43,25 @@ class IQRCodeProviderTest extends TestCase
     /**
      * @return void
      */
+    public function testTotpUriIsCorrectNoIssuer()
+    {
+        $qr = new TestQrProvider();
+
+        /**
+         * The library specifies the issuer is null by default however in PHP 8.1
+         * there is a deprecation warning for passing null as a string argument to rawurlencode
+         */
+
+        $tfa = new TwoFactorAuth(null, 6, 30, 'sha1', $qr);
+        $data = $this->DecodeDataUri($tfa->getQRCodeImageAsDataUri('Test&Label', 'VMR466AB62ZBOKHE'));
+        $this->assertEquals('test/test', $data['mimetype']);
+        $this->assertEquals('base64', $data['encoding']);
+        $this->assertEquals('otpauth://totp/Test%26Label?secret=VMR466AB62ZBOKHE&issuer=&period=30&algorithm=SHA1&digits=6@200', $data['data']);
+    }
+
+    /**
+     * @return void
+     */
     public function testGetQRCodeImageAsDataUriThrowsOnInvalidSize()
     {
         $qr = new TestQrProvider();
