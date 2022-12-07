f1e73aab3ab76d61575217beca5e19fa1b8400ce
Nicolas CARPi <nico-git@deltablot.email>
2022-12-07 23:29:04 +0100

    fix the testsDependency folder

--- testsDependency/EndroidQRCodeTest.php
+++ testsDependency/EndroidQRCodeTest.php
@@ -3,6 +3,7 @@
 namespace TestsDependency;
 
 use PHPUnit\Framework\TestCase;
+use RobThree\Auth\Algorithm;
 use RobThree\Auth\Providers\Qr\EndroidQrCodeProvider;
 use RobThree\Auth\Providers\Qr\HandlesDataUri;
 use RobThree\Auth\TwoFactorAuth;
@@ -11,10 +12,10 @@ class EndroidQRCodeTest extends TestCase
 {
     use HandlesDataUri;
 
-    public function testDependency()
+    public function testDependency(): void
     {
         $qr = new EndroidQrCodeProvider();
-        $tfa = new TwoFactorAuth('Test&Issuer', 6, 30, 'sha1', $qr);
+        $tfa = new TwoFactorAuth('Test&Issuer', 6, 30, Algorithm::Sha1, $qr);
         $data = $this->DecodeDataUri($tfa->getQRCodeImageAsDataUri('Test&Label', 'VMR466AB62ZBOKHE'));
         $this->assertEquals('image/png', $data['mimetype']);
         $this->assertEquals('base64', $data['encoding']);
