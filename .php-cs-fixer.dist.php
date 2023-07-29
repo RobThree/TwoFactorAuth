137df4dd3cd998e7096705cd0b3aed17b4c25d53
William Hall <github@willpoweredinc.co.uk>
2023-07-29 15:46:20 +0100

    🔧 remove deprecated rule https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/pull/7066

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -39,7 +39,6 @@ return $config->setRules(array(
     'semicolon_after_instruction' => true,
     'short_scalar_cast' => true,
     'simplified_null_return' => true,
-    'single_blank_line_before_namespace' => true,
     'single_class_element_per_statement' => true,
     'single_line_comment_style' => true,
     'single_quote' => true,
